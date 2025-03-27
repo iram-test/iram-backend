3b0922d7b7ebbef42243752cfde46cccc97d1164
hhuziuk <huziukwork@gmail.com>
2025-03-27 10:09:41 +0100

    implemented minio S3 bucket

--- src/infrastructure/controllers/step-controller.ts
+++ src/infrastructure/controllers/step-controller.ts
@@ -2,16 +2,65 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import StepService from "../services/step-service";
 import logger from "../../tools/logger";
 import { CustomError } from "../../tools/custom-error";
-import { pipeline } from "stream";
 import path from "node:path";
-import * as fs from "node:fs";
+
 import {
   CreateStepDTOSchema,
   UpdateStepDTOSchema,
 } from "../../application/validation/dto-validation/step-dto-schema";
 import { z } from "zod";
+import * as Minio from 'minio';
+import {config} from "../../configs";
+import {PassThrough} from "stream";
 
-const UPLOAD_DIR = path.join(process.cwd(), "uploads");
+const minioClient = new Minio.Client({
+  endPoint: config.minio.endPoint,
+  port: config.minio.port,
+  useSSL: config.minio.useSSL,
+  accessKey: config.minio.accessKey,
+  secretKey: config.minio.secretKey
+});
+
+async function checkAndCreateBucket() {
+  try {
+    logger.info(`Checking if bucket ${config.minio.bucketName} exists...`);
+    const bucketExists = await minioClient.bucketExists(config.minio.bucketName);
+    logger.info(`Bucket ${config.minio.bucketName} exists: ${bucketExists}`);
+    if (!bucketExists) {
+      logger.info(`Creating bucket ${config.minio.bucketName}...`);
+      await minioClient.makeBucket(config.minio.bucketName, 'us-east-1');
+      logger.info(`Bucket ${config.minio.bucketName} created successfully`);
+    }
+  } catch (err: any) {
+    logger.error(`Error checking/creating bucket: ${err.message}`);
+    logger.error(`Error details: ${err.stack}`); // Add this line
+    throw new Error(`Failed to initialize MinIO: ${err.message}`);
+  }
+}
+checkAndCreateBucket().catch(error => {
+  logger.error("MinIO bucket initialization failed:", error);
+  process.exit(1);
+});
+
+const generateUniqueFilename = (stepId: string, filename: string): string => {
+  const fileExtension = path.extname(filename);
+  return `${stepId}-${Date.now()}${fileExtension}`;
+};
+
+const uploadToMinIO = async (fileStream: NodeJS.ReadableStream, filename: string, fileSize: number): Promise<string> => {
+  try {
+    const passThrough = new PassThrough();
+    fileStream.pipe(passThrough);
+
+    await minioClient.putObject(config.minio.bucketName, filename, passThrough, fileSize);
+    const publicUrl = `${config.minio.publicUrl}/${config.minio.bucketName}/${filename}`;
+    logger.info(`File uploaded successfully to MinIO: ${filename}`);
+    return publicUrl;
+  } catch (error: any) {
+    logger.error(`Error uploading file to MinIO: ${error.message}`);
+    throw new Error(`Failed to upload to MinIO: ${error.message}`);
+  }
+};
 
 export const addStep = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
@@ -22,8 +71,8 @@ export const addStep = async (request: FastifyRequest, reply: FastifyReply) => {
   } catch (error) {
     if (error instanceof z.ZodError) {
       reply
-        .code(400)
-        .send({ message: "Validation error", errors: error.errors });
+          .code(400)
+          .send({ message: "Validation error", errors: error.errors });
     } else {
       logger.error(`Error creating step: ${error}`);
       reply.status(500).send({ message: "Error creating step" });
@@ -32,8 +81,8 @@ export const addStep = async (request: FastifyRequest, reply: FastifyReply) => {
 };
 
 export const getStepById = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
+    request: FastifyRequest,
+    reply: FastifyReply,
 ) => {
   try {
     const { stepId } = request.params as { stepId: string };
@@ -50,8 +99,8 @@ export const getStepById = async (
 };
 
 export const updateStep = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
+    request: FastifyRequest,
+    reply: FastifyReply,
 ) => {
   try {
     const { stepId } = request.params as { stepId: string };
@@ -59,8 +108,8 @@ export const updateStep = async (
 
     if (stepId !== stepDto.stepId) {
       return reply
-        .status(400)
-        .send({ message: "Step ID in path and body do not match" });
+          .status(400)
+          .send({ message: "Step ID in path and body do not match" });
     }
 
     const updatedStep = await StepService.update(stepId, stepDto);
@@ -68,11 +117,11 @@ export const updateStep = async (
   } catch (error) {
     if (error instanceof z.ZodError) {
       reply
-        .code(400)
-        .send({ message: "Validation error", errors: error.errors });
+          .code(400)
+          .send({ message: "Validation error", errors: error.errors });
     } else {
       logger.error(
-        `Error during update step with id ${request.params}: ${error}`,
+          `Error during update step with id ${request.params}: ${error}`,
       );
       reply.status(500).send({ message: "Error updating step" });
     }
@@ -80,8 +129,8 @@ export const updateStep = async (
 };
 
 export const deleteStep = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
+    request: FastifyRequest,
+    reply: FastifyReply,
 ) => {
   try {
     const { stepId } = request.params as { stepId: string };
@@ -98,8 +147,8 @@ export const deleteStep = async (
 };
 
 export const getStepsByTestCaseId = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
+    request: FastifyRequest,
+    reply: FastifyReply,
 ) => {
   try {
     const { testCaseId } = request.params as { testCaseId: string };
@@ -116,94 +165,82 @@ export const getStepsByTestCaseId = async (
 };
 
 export const uploadImage = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
+    request: FastifyRequest,
+    reply: FastifyReply,
 ) => {
   try {
     const { stepId } = request.params as { stepId: string };
     const data = await request.file({ limits: { fileSize: 10000000 } });
+
     if (!data) {
-      reply.status(400).send({ message: "No file uploaded" });
-      return;
+      return reply.status(400).send({ message: "No file uploaded" });
     }
-    const filename = data.filename;
-    const fileExtension = path.extname(filename);
-    const newFilename = `${stepId}-${Date.now()}${fileExtension}`;
-    const uploadPath = path.join(UPLOAD_DIR, newFilename);
-    fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 
-    await new Promise((resolve, reject) => {
-      pipeline(data.file, fs.createWriteStream(uploadPath), (err) => {
-        if (err) {
-          logger.error(`Pipeline failed: ${err}`);
-          reject(err);
-        } else {
-          logger.info("Pipeline succeeded");
-          resolve(true);
-        }
-      });
-    });
+    const filename = generateUniqueFilename(stepId, data.filename);
 
-    const baseUrl = process.env.BASE_URL || "http://localhost:3002";
-    const imageUrl = `${baseUrl}/uploads/${newFilename}`;
+    const imageUrl = await uploadToMinIO(data.file, filename, data.file.readableLength || data.file.bytesRead);
 
-    // Оновлюємо поле для звичайних зображень кроку
     const updatedStep = await StepService.uploadImage(stepId, imageUrl);
     reply.status(200).send(updatedStep);
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      reply
-        .code(400)
-        .send({ message: "Validation error", errors: error.errors });
-    } else {
-      logger.error(`Error uploading image: ${error}`);
-      reply.status(500).send({ message: "Error uploading image" });
-    }
+
+  } catch (error: any) {
+    logger.error(`Error uploading image: ${error.message}`);
+    reply.status(500).send({ message: `Error uploading image: ${error.message}` });
   }
 };
 
 export const uploadExpectedImage = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
+    request: FastifyRequest,
+    reply: FastifyReply,
 ) => {
   try {
     const { stepId } = request.params as { stepId: string };
     const data = await request.file({ limits: { fileSize: 10000000 } });
+
     if (!data) {
-      reply.status(400).send({ message: "No file uploaded" });
-      return;
+      return reply.status(400).send({ message: "No file uploaded" });
     }
-    const filename = data.filename;
-    const fileExtension = path.extname(filename);
-    const newFilename = `${stepId}-${Date.now()}${fileExtension}`;
-    const uploadPath = path.join(UPLOAD_DIR, newFilename);
-    fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 
-    await new Promise((resolve, reject) => {
-      pipeline(data.file, fs.createWriteStream(uploadPath), (err) => {
-        if (err) {
-          logger.error(`Pipeline failed: ${err}`);
-          reject(err);
-        } else {
-          logger.info("Pipeline succeeded");
-          resolve(true);
-        }
-      });
-    });
-
-    const baseUrl = process.env.BASE_URL || "http://localhost:3002";
-    const imageUrl = `${baseUrl}/uploads/${newFilename}`;
+    const filename = generateUniqueFilename(stepId, data.filename);
+    const imageUrl = await uploadToMinIO(data.file, filename, data.file.readableLength || data.file.bytesRead);
 
     const updatedStep = await StepService.uploadExpectedImage(stepId, imageUrl);
     reply.status(200).send(updatedStep);
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      reply
-        .code(400)
-        .send({ message: "Validation error", errors: error.errors });
-    } else {
-      logger.error(`Error uploading expected image: ${error}`);
-      reply.status(500).send({ message: "Error uploading expected image" });
-    }
+
+  } catch (error: any) {
+    logger.error(`Error uploading expected image: ${error.message}`);
+    reply.status(500).send({ message: `Error uploading expected image: ${error.message}` });
+  }
+};
+
+export const deleteImage = async (
+    request: FastifyRequest,
+    reply: FastifyReply,
+) => {
+  try {
+    const { stepId } = request.params as { stepId: string };
+    const { imageUrl } = request.body as { imageUrl: string };
+
+    await StepService.deleteImage(stepId, imageUrl);
+    reply.status(204).send();
+  } catch (error: any) {
+    logger.error(`Error deleting image: ${error.message}`);
+    reply.status(500).send({ message: `Error deleting image: ${error.message}` });
+  }
+};
+
+export const deleteExpectedImage = async (
+    request: FastifyRequest,
+    reply: FastifyReply,
+) => {
+  try {
+    const { stepId } = request.params as { stepId: string };
+    const { imageUrl } = request.body as { imageUrl: string };
+
+    await StepService.deleteExpectedImage(stepId, imageUrl);
+    reply.status(204).send();
+  } catch (error: any) {
+    logger.error(`Error deleting expected image: ${error.message}`);
+    reply.status(500).send({ message: `Error deleting expected image: ${error.message}` });
   }
 };
--- src/infrastructure/routes/step.routes.ts
+++ src/infrastructure/routes/step.routes.ts
@@ -7,6 +7,8 @@ import {
   getStepsByTestCaseId,
   uploadImage,
   uploadExpectedImage,
+  deleteImage,
+  deleteExpectedImage
 } from "../controllers/step-controller";
 import { authorize } from "../middlewares/authorization-middleware";
 import { UserRole } from "../../domain/entities/enums/user-role";
@@ -14,58 +16,74 @@ import { UserRole } from "../../domain/entities/enums/user-role";
 export async function stepRoutes(fastify: FastifyInstance) {
   const roles = [UserRole.MANAGER, UserRole.USER, UserRole.ADMIN];
   fastify.post(
-    "/test-cases/:testCaseId/steps",
-    {
-      preHandler: [authorize(roles)],
-    },
-    addStep,
+      "/test-cases/:testCaseId/steps",
+      {
+        preHandler: [authorize(roles)],
+      },
+      addStep,
   );
 
   fastify.post(
-    "/test-cases/:testCaseId/steps/:stepId/upload-image",
-    {
-      preHandler: [authorize(roles)],
-    },
-    uploadImage,
+      "/test-cases/:testCaseId/steps/:stepId/upload-image",
+      {
+        preHandler: [authorize(roles)],
+      },
+      uploadImage,
   );
 
   fastify.post(
-    "/test-cases/:testCaseId/steps/:stepId/upload-expected-image",
-    {
-      preHandler: [authorize(roles)],
-    },
-    uploadExpectedImage,
-  );
-
-  fastify.get(
-    "/test-cases/:testCaseId/steps",
-    {
-      preHandler: [authorize(roles)],
-    },
-    getStepsByTestCaseId,
-  );
-
-  fastify.get(
-    "/steps/:stepId",
-    {
-      preHandler: [authorize(roles)],
-    },
-    getStepById,
-  );
-
-  fastify.put(
-    "/steps/:stepId",
-    {
-      preHandler: [authorize(roles)],
-    },
-    updateStep,
+      "/test-cases/:testCaseId/steps/:stepId/upload-expected-image",
+      {
+        preHandler: [authorize(roles)],
+      },
+      uploadExpectedImage,
   );
 
   fastify.delete(
-    "/steps/:stepId",
-    {
-      preHandler: [authorize(roles)],
-    },
-    deleteStep,
+      "/steps/:stepId/image",
+      {
+        preHandler: [authorize(roles)],
+      },
+      deleteImage,
+  );
+
+  fastify.delete(
+      "/steps/:stepId/expected-image",
+      {
+        preHandler: [authorize(roles)],
+      },
+      deleteExpectedImage,
+  );
+
+  fastify.get(
+      "/test-cases/:testCaseId/steps",
+      {
+        preHandler: [authorize(roles)],
+      },
+      getStepsByTestCaseId,
+  );
+
+  fastify.get(
+      "/steps/:stepId",
+      {
+        preHandler: [authorize(roles)],
+      },
+      getStepById,
+  );
+
+  fastify.put(
+      "/steps/:stepId",
+      {
+        preHandler: [authorize(roles)],
+      },
+      updateStep,
+  );
+
+  fastify.delete(
+      "/steps/:stepId",
+      {
+        preHandler: [authorize(roles)],
+      },
+      deleteStep,
   );
 }
